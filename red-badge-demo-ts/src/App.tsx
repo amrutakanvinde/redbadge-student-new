c588d744c8a6a1d683b7e937ba5069b533778524
Amruta Ray <amrutaray@Amrutas-MacBook-Air.local>
2021-03-23 19:47:31 -0400

    Red badge PT initial push

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import PropsExample from './components/PropsExample'
+import PropsMapping from "./components/PropsMapping";
 
 function App() {
-  return <div></div>;
+  const visitedPlaces = ["Hawaii", "Chicago", "Bangalore", "Manali"]
+  return (
+    <div>
+        <PropsExample name="Tom" business="MySpace"/>
+        <PropsMapping visited = {visitedPlaces}/>
+    </div>
+  );
 }
 
 export default App;
